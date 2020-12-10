8a0cdf0e7ee3511a0969bb1c5f5df44fed4e1172
Pirasanna Ravi <pirasanna.ravi@live.com>
2020-12-10 13:57:52 -0500

    First Commit

---
+++ HelloWorld/test.cs
@@ -0,0 +1,14 @@
+public class Crop
+{
+    public string name { get; set; }
+
+    public float currentStage { get; set; }
+
+    public float maxStage { get; set; }
+
+    public float currentLifeTime { get; set; }
+
+    public float nutritionalValue { get; set; }
+
+    public float timeToGrow { get; set; }
+}
